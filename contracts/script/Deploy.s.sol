83c3b79fa975dc7a6758a051b21261c7d3573607
kushSharma924 <kushsharma924@gmail.com>
2026-02-18 01:49:29 -0600

    Updated deployment script to not allow defaults

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -6,21 +6,9 @@ import {Multisig} from "../src/Multisig.sol";
 
 contract DeployScript is Script {
     function run() external returns (Multisig multisig) {
-        // Defaults target Anvil account[0]. Override via env vars for other networks.
-        uint256 deployerPrivateKey = vm.envOr(
-            "PRIVATE_KEY",
-            uint256(
-                0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
-            )
-        );
-
-        address[] memory defaultOwners = new address[](3);
-        defaultOwners[0] = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-        defaultOwners[1] = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
-        defaultOwners[2] = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
-
-        address[] memory owners = vm.envOr("OWNERS", ",", defaultOwners);
-        uint256 threshold = vm.envOr("THRESHOLD", uint256(2));
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        address[] memory owners = vm.envAddress("OWNERS", ",");
+        uint256 threshold = vm.envUint("THRESHOLD");
 
         vm.startBroadcast(deployerPrivateKey);
         multisig = new Multisig(owners, threshold);
